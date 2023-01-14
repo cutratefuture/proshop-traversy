4cc0216e74b68e1a68715c695bd6a5b50f158758
cutratefuture <christian@cutratefuture.com>
2023-01-13 19:30:38 -0800

    updated to react18 and eov#7

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+const container = document.getElementById("root"); 
+const root = createRoot(container);
+
+root.render(
+  // ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  // );
 );
 
 // If you want your app to work offline and load faster, you can change
