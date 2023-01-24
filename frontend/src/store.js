dab8d71259e1c07c59d25f3df2a1b0b9b2d4b395
cutratefuture <christian@cutratefuture.com>
2023-01-24 15:17:31 -0800

    ev# 31 -added productdetails reducer actions constant

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
-import { productListReducer } from './reducers/productReducers'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
 
 const store = configureStore({
-    reducer: { productList: productListReducer, },
-    preloadedState: {},
-    middleware: [thunk],
+  reducer: {
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+  },
+  preloadedState: {},
+  middleware: [thunk],
 });
 
 export default store;
