a478eaf7fc56de7dfacb24c6f983f99184785dea
cutratefuture <christian@cutratefuture.com>
2023-01-25 13:36:12 -0800

    ev#33 - added cart action, constatns, reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,12 +4,19 @@ import {
   productListReducer,
   productDetailsReducer,
 } from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
+
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
 
 const store = configureStore({
   reducer: {
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    cart: cartReducer,
   },
+  cart: { cartItems: cartItemsFromStorage },
   preloadedState: {},
   middleware: [thunk],
 });
